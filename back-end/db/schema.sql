ec3d91d9b73d8f53c4f23aa58f350d6e10d01244
Rose Durandisse <rosedurandisse@pursuit.org>
2022-05-13 12:53:30 -0400

    Added not null to the activity table

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -8,11 +8,11 @@ CREATE TABLE activity (
     id SERIAL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
     description TEXT,
-    street_address TEXT,
-    city TEXT,
-    state TEXT,
-    zip_code INT,
-    category TEXT,
+    street_address TEXT NOT NULL,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+    zip_code INT NOT NULL,
+    category TEXT NOT NULL,
     image TEXT
 );
 
