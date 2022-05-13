1af9fded7ad202300478d1a238951a6ac181ed89
Jalamang <Jalamangjanka@gmail.com>
2022-05-13 14:34:09 -0400

    added navbar, hamburger Menu using bootstrap

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
 import 'bootstrap/dist/js/bootstrap.js';
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
