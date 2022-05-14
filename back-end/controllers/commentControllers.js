895b8f9b67733c962a1a675691bb9d14d05dd358
Rose Durandisse <rosedurandisse@pursuit.org>
2022-05-13 21:22:08 -0400

    Updated comment paths and queries

--- back-end/controllers/commentControllers.js
+++ back-end/controllers/commentControllers.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
-const { getActivityComments } = require("../queries/comments");
+const {
+  getActivityComments,
+  addComment,
+  updateComment,
+  deleteComment,
+} = require("../queries/comments");
 
 const comments = express.Router({ mergeParams: true });
 
@@ -9,4 +14,24 @@ comments.get("/", async (req, res) => {
   res.status(200).json(comments);
 });
 
+comments.post("/", async (req, res) => {
+  const addedComments = await addComment(req.body);
+  res.status(200).json(addedComments);
+});
+
+comments.put("/:id", async (req, res) => {
+  const comment = await updateComment(req.params.id, req.body);
+  res.status(200).json(comment);
+});
+
+comments.delete("/:id", async (req, res) => {
+  const comment = await deleteComment(req.params.id);
+  if (comment) {
+    res.status(200).json(comment);
+  } else {
+    res
+      .status(404)
+      .json({ error: `review with id of ${id} could not be deleted` });
+  }
+});
 module.exports = comments;
