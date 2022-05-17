101e4fda41e381000a70878950798ee39ce2f6ca
Rose Durandisse <rosedurandisse@pursuit.org>
2022-05-17 12:10:36 -0400

    Added details to the comment form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import Activity from "./components/Activity";
 import NewPost from "./components/NewPost";
 import EditPost from "./components/EditPost";
 import Navbar from "./components/Navbar";
+import CommentForm from "./components/CommentForm";
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
         <Route path="/activity/:id" element={<Activity />} />
         <Route path="/activity/new" element={<NewPost />} />
         <Route path="/activity/:id/edit" element={<EditPost />} />
+        <Route path="/comments/:id/edit" element={<CommentForm />} />
+        {/* The following will be a route for editing comments page */}
+        <Route></Route>
       </Routes>
     </div>
   );
